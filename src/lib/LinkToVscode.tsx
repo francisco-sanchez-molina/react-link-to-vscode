d2df2da19d8a041485f34008d68e2acad583609a
Francisco Sánchez Molina <psm1984@gmail.com>
2022-07-26 12:28:07 +0200

    update

--- src/lib/LinkToVscode.tsx
+++ src/lib/LinkToVscode.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-const process = require("process");
+import process from "process";
 
 const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
