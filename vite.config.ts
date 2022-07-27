209dc642e155c13114385c178e483c29851a102e
Francisco Sánchez Molina <psm1984@gmail.com>
2022-07-27 19:50:10 +0200

    first valid version

--- vite.config.ts
+++ vite.config.ts
@@ -18,13 +18,12 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react', 'react-dom'],
+      external: ['react'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM',
         },
       }
     }
